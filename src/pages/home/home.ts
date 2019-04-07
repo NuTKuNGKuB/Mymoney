d31ac2f34a9f8ab0d50822090f77d76fa1e9a1ea
nitipong santiphapchai <nitipong.za1997@gmail.com>
2019-04-07 11:04:39 +0700

    adddata

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,85 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
-
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite/ngx';
+import { AdddataPage } from '../adddata/adddata';
+import { EditdataPage } from '../editdata/editdata';
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
-
+  expenses:any=[];
+  totalIncome=0;
+  totalExpense=0;
+  balance=0;
+  constructor(public navCtrl: NavController, public sqlite:SQLite) {
   }
+  ionViewDidLoad(){
+    this.getData();
+  }
+  ionViewWillEnter(){
+    this.getData();
+  }
+  getData(){
+    this.sqlite.create({
+      name: 'ionicdb.db',
+      location: 'default'
+    }).then(
+     (db:SQLiteObject)=>{
+        db.executeSql('CREATE TABLE IF NOT EXISTS expense (rowid INTEGER PRIMARY KEY, date TEXT, type TEXT, description TEXT, amount INTEGER)')
+        .then(res=>console.log('Executed SQL'))
+        .catch(e=>console.log(e));
+        db.executeSql('SELECT * FROM exepense ORDER BY rowid DESC')
+        .then(res=>{
+          this.expenses=[];
+          for(var i=0;i<res.rows.length;i++){
+            //อ่านค่าทุกแถวมาใส่ใน object
+            this.expenses.push({
+              rowid:res.rows.item(i).rowid,
+              date:res.rows.item(i).date,
+              type:res.rows.item(i).type,
+              description:res.rows.item(i).description,
+              amount:res.rows.item(i).amount,
+            });
+          }
+        })
+        .catch(e=>console.log(e));
 
+        db.executeSql('SELECT SUM(amount) AS totalIncome FROM expense WHERE type="EXPENSE"')
+        .then(res=>{
+          if(res.rows.length>0){
+            this.totalExpense = parseInt(res.rows.item(0).totalExpense);
+            this.balance = this.totalIncome - this.totalExpense;
+          }
+        })
+        .catch(e=>console.log(e));
+
+     } 
+    );
+  }
+    addData(){
+      this.navCtrl.push(AdddataPage);
+    }
+    editData(rowid){
+      this.navCtrl.push(EditdataPage, {rowid : rowid});
+    }
+    deletData(rowid){
+      this.sqlite.create(
+        {
+          name: 'ionicdb.db',
+          location: 'default'
+        }
+      )
+        .then(
+          (db:SQLiteObject)=>{
+            db.executeSql('DELETE FROM expense WHERE row id=?',[rowid])
+              .then(res=>{
+                console.log(res);
+                this.getData();
+              })
+              .catch(e=>console.log(e));
+          }
+        )
+        .catch(e=>console.log(e));
+    }
 }
